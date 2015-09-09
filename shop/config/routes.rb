2952c1954eebd0385fb9c8e55fa3b653d79f2f75
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-09 20:10:59 +0000

    Add admin account and creat first step to activate account via email

--- shop/config/routes.rb
+++ shop/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-
-
+  get 'users'   => 'users#index'
+  
   resources :users
 end
