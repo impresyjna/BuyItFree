3e637b7a4916701a3220cab7919ed4540a31450b
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-10 19:25:35 +0000

    Revert "To revert commit"
    This reverts commit 989843a443880405af41f5b941dd2d4a0a333f34.

--- shop/db/schema.rb
+++ shop/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910184821) do
+ActiveRecord::Schema.define(version: 20150909231510) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(version: 20150910184821) do
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.string   "remember_digest"
-    t.string   "reset_digest"
-    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
