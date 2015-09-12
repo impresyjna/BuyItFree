0bf8219611166751cacefaf37d538b2ce48c943e
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-12 10:23:12 +0000

    Left side menu with categories

--- shop/app/controllers/static_pages_controller.rb
+++ shop/app/controllers/static_pages_controller.rb
@@ -1,23 +1,18 @@
 class StaticPagesController < ApplicationController
   def home
     @good = current_user.goods.build if logged_in?
-    @categories = Category.all 
   end
 
   def help
-        @categories = Category.all 
   end
 
   def rules
-        @categories = Category.all 
   end
 
   def about
-        @categories = Category.all 
   end
   
   def contact
-        @categories = Category.all 
   end
   
 end
