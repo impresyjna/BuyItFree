dd752f24286a2e27ad10b9cd447e3bc3578c6a05
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-15 21:40:44 +0000

    Changed messages language while logging in

--- shop/app/controllers/sessions_controller.rb
+++ shop/app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def new
   end
   
-  def create
+def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       if user.activated?
@@ -10,13 +10,13 @@ class SessionsController < ApplicationController
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
       else
-        message  = "Account not activated. "
-        message += "Check your email for the activation link."
+        message  = "Konto nieaktywne. "
+        message += "Sprawdź swój email czy nie ma maila aktywacyjnego. "
         flash[:warning] = message
         redirect_to root_url
       end
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Niepoprawna kombinacja email/hasło. '
       render 'new'
     end
   end
