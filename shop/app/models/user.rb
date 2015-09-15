7c7a45d75f0c594dfa2a33f3dc22a29fa13275cd
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-15 18:02:38 +0000

    Repair sign up form and add category to admin

--- shop/app/models/user.rb
+++ shop/app/models/user.rb
@@ -12,8 +12,8 @@ class User < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
     has_secure_password
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-    validates_inclusion_of :account_type, :in => OPTIONS
-
+    validates :account_type, presence: true 
+    
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
