614455162334451127d627f28c4b15f8eca19b12
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-16 23:34:10 +0000

    Name, surname and address in customer validation

--- shop/app/models/customer.rb
+++ shop/app/models/customer.rb
@@ -3,8 +3,12 @@ class Customer < ActiveRecord::Base
   
     #validates :name,  presence: true, length: { maximum: 50 }
     VALID_POST_CODE_REGEX = /\d{2}-\d{3}\z/
-    VALID_CITY_REGEX = /\A\D+\z/
+    VALID_WORD_REGEX = /\A\D+\z/
     validates :post_code, presence: true, format: { with: VALID_POST_CODE_REGEX }
-    validates :city, presence: true, format: {with: VALID_CITY_REGEX }
+    validates :city, presence: true, format: {with: VALID_WORD_REGEX }
+    validates :name, presence: true, format: {with: VALID_WORD_REGEX }
+    validates :surname, presence: true, format: {with: VALID_WORD_REGEX }
+    validates :address, presence: true
+    
   
 end
