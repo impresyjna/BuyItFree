813cf05274afcc506e80cb325ad9d664ea20eb72
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-16 23:35:08 +0000

    In customer add length validation

--- shop/app/models/customer.rb
+++ shop/app/models/customer.rb
@@ -5,10 +5,10 @@ class Customer < ActiveRecord::Base
     VALID_POST_CODE_REGEX = /\d{2}-\d{3}\z/
     VALID_WORD_REGEX = /\A\D+\z/
     validates :post_code, presence: true, format: { with: VALID_POST_CODE_REGEX }
-    validates :city, presence: true, format: {with: VALID_WORD_REGEX }
-    validates :name, presence: true, format: {with: VALID_WORD_REGEX }
-    validates :surname, presence: true, format: {with: VALID_WORD_REGEX }
-    validates :address, presence: true
+    validates :city, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
+    validates :name, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
+    validates :surname, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
+    validates :address, presence: true, length: { maximum: 100 }
     
   
 end
