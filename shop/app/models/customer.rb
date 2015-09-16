0ddf792babf664cb1976d55e1be1c6a2efc1e961
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-16 23:27:48 +0000

    City validation

--- shop/app/models/customer.rb
+++ shop/app/models/customer.rb
@@ -3,6 +3,8 @@ class Customer < ActiveRecord::Base
   
     #validates :name,  presence: true, length: { maximum: 50 }
     VALID_POST_CODE_REGEX = /\d{2}-\d{3}\z/
+    VALID_CITY_REGEX = /\A\D+\z/
     validates :post_code, presence: true, format: { with: VALID_POST_CODE_REGEX }
+    validates :city, presence: true, format: {with: VALID_CITY_REGEX }
   
 end
