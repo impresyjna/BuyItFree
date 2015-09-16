2235b785730ca6063b05e2140977de829c485721
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-16 23:38:49 +0000

    Telephone validation in customer

--- shop/app/models/customer.rb
+++ shop/app/models/customer.rb
@@ -1,7 +1,6 @@
 class Customer < ActiveRecord::Base
     belongs_to :user
   
-    #validates :name,  presence: true, length: { maximum: 50 }
     VALID_POST_CODE_REGEX = /\d{2}-\d{3}\z/
     VALID_WORD_REGEX = /\A\D+\z/
     validates :post_code, presence: true, format: { with: VALID_POST_CODE_REGEX }
@@ -9,6 +8,7 @@ class Customer < ActiveRecord::Base
     validates :name, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
     validates :surname, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
     validates :address, presence: true, length: { maximum: 100 }
+    validates :telephone, presence: true, numericality: true, length: { :minimum => 7, :maximum => 15 }
     
   
 end
