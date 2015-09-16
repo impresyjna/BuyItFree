88df319719c5718f9cd20aa85a63f2d0ee5eee46
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-16 23:44:28 +0000

    Validation for seller like in customer

--- shop/app/models/seller.rb
+++ shop/app/models/seller.rb
@@ -1,3 +1,12 @@
 class Seller < ActiveRecord::Base
   belongs_to :user
+  
+    VALID_POST_CODE_REGEX = /\d{2}-\d{3}\z/
+    VALID_WORD_REGEX = /\A\D+\z/
+    validates :post_code, presence: true, format: { with: VALID_POST_CODE_REGEX }
+    validates :city, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
+    validates :name, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
+    validates :surname, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
+    validates :address, presence: true, length: { maximum: 100 }
+    validates :telephone, presence: true, numericality: true, length: { :minimum => 7, :maximum => 15 }
 end
