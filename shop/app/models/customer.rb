5ba03b9de77d73b36046a3b1c76d9b66656dbcdd
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-16 23:14:11 +0000

    Post code validation

--- shop/app/models/customer.rb
+++ shop/app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ActiveRecord::Base
-  belongs_to :user
+    belongs_to :user
+  
+    #validates :name,  presence: true, length: { maximum: 50 }
+    VALID_POST_CODE_REGEX = /\d{2}-\d{3}\z/
+    validates :post_code, presence: true, format: { with: VALID_POST_CODE_REGEX }
+  
 end
