879764404e68268232f1b11be5c6af9686362b04
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-17 11:29:49 +0000

    First account validation

--- shop/app/models/customer.rb
+++ shop/app/models/customer.rb
@@ -3,12 +3,14 @@ class Customer < ActiveRecord::Base
   
     VALID_POST_CODE_REGEX = /\d{2}-\d{3}\z/
     VALID_WORD_REGEX = /\A\D+\z/
+
     validates :post_code, presence: true, format: { with: VALID_POST_CODE_REGEX }
     validates :city, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
     validates :name, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
     validates :surname, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
     validates :address, presence: true, length: { maximum: 100 }
     validates :telephone, numericality: true, length: { :minimum => 7, :maximum => 15 }
-    
+    validates :company_city, format: {with: VALID_WORD_REGEX, message: "Tylko litery dozwolone" }, length: { maximum: 50 }
+
   
 end
--- shop/app/models/seller.rb
+++ shop/app/models/seller.rb
@@ -3,10 +3,12 @@ class Seller < ActiveRecord::Base
   
     VALID_POST_CODE_REGEX = /\d{2}-\d{3}\z/
     VALID_WORD_REGEX = /\A\D+\z/
+    VALID_ACCOUNT_REGEX = /\A[0-9]{26}\z/
     validates :post_code, presence: true, format: { with: VALID_POST_CODE_REGEX }
     validates :city, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
     validates :name, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
     validates :surname, presence: true, format: {with: VALID_WORD_REGEX }, length: { maximum: 50 }
     validates :address, presence: true, length: { maximum: 100 }
     validates :telephone, numericality: true, length: { :minimum => 7, :maximum => 15 }
+    validates :account_number, presence: true, format: {with: VALID_ACCOUNT_REGEX, message: "Niepoprawny numer konta" }
 end
