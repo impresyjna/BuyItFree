4aac5056de8448c6abab712ee72180ea34eb9d8c
Asia <joanna.furmaniak@student.put.poznan.pl>
2015-09-18 12:20:20 +0000

    How many validations

--- shop/app/models/good.rb
+++ shop/app/models/good.rb
@@ -3,8 +3,12 @@ class Good < ActiveRecord::Base
     belongs_to :user
     belongs_to :category
     
+    VALID_FLOAT_REGEX = /\A[0-9\,]+\z/
+    
     validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
     
     validates :title, presence: true, length: { maximum: 150 }
     validates :description, presence: true
+    validates :price, presence: true, format: {with: VALID_FLOAT_REGEX }
+    validates :how_many, :numericality => { :greater_than => 0 }
 end
